332120bf67afe63092829854e1a1137aeaeaabdb
Netanel Nadav <natinadav932@gmail.com>
2022-01-27 18:55:03 +0200

    like page

--- src/cmps/LikedSongsHero.jsx
+++ src/cmps/LikedSongsHero.jsx
@@ -4,16 +4,20 @@ import { connect } from "react-redux";
 function _LikedSongsHero({ user }) {
 
     console.log(user)
-    if(!user) return <h1>Loading...</h1>
-    const {likedSongs, backgroundColor, imgUrl} = user
+    if (!user) return <h1>Loading...</h1>
+    const { likedSongs } = user
+    const backgroundColor = 'rgb(80, 56, 160)'
     const transperent = 'rgb(0 0 0 / 0%)'
     return (
-        <section className="station-header hero" style={{backgroundImage: `linear-gradient(181deg, ${backgroundColor}, ${transperent})`}}>
-            <div className="hero-container flex align-center">
-                <div className="user-info">
-
-                    <h1>{user?.username} liked songs</h1>
-                    <h3>{user?.likedSongs.length} Songs </h3>
+        <section className="like-hero" style={{ backgroundImage: `linear-gradient(181deg, ${backgroundColor}, ${transperent})` }}>
+            <div className="details flex align-center">
+                <div className="img-container">
+                    <img src='https://res.cloudinary.com/dvxuxsyoe/image/upload/v1643297851/fosgdv7tdjci8x5bfjaz.png' />
+                </div>
+                <div className="user-details flex column">
+                    <small>Playlist</small>
+                    <h1>{user?.username}liked songs</h1>
+                    <h3>{user?.likedSongs.length}Songs</h3>
                 </div>
             </div>
         </section>
@@ -22,11 +26,11 @@ function _LikedSongsHero({ user }) {
 
 function mapStateToProps({ userModule }) {
     return {
-      user: userModule.user
+        user: userModule.user
     };
-  }
-  
-  const mapDispatchToProps = {
-  };
-  
-  export const LikedSongsHero = connect(mapStateToProps, mapDispatchToProps)(_LikedSongsHero);
+}
+
+const mapDispatchToProps = {
+};
+
+export const LikedSongsHero = connect(mapStateToProps, mapDispatchToProps)(_LikedSongsHero);
